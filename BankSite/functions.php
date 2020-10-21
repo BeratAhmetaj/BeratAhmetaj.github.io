e4da61f42b89bb7bc7c38ea285314de9675a8e8b
Berat Ahmetaj <beratahmetaj02@gmail.com>
2020-10-21 21:38:38 +0200

    +comments for revisions

--- BankSite/functions.php
+++ BankSite/functions.php
@@ -23,27 +23,37 @@ function passwordCheck($pass, $passRepeat){
 }
 
 function usernameTaken($username, $connect, $email){
+    // sql we want to inject
     $sql = "SELECT * FROM users WHERE Username = ? OR Email = ?;";
 
+    //statement 
     $stmt = mysqli_stmt_init($connect);
 
-    if( !mysqli_stmt_prepare($smtm, $sqp)){
+    //if statemant is not prepared to take the statement and sql than we go to header location
+    if( !mysqli_stmt_prepare($smtm, $sql)){
         header('location : login.html?error=SQL_Failed');
         exit();
     }
 
+    //mysqli statemnt bind parameteres (we bind the statement, we type an s for every string we would like to attach, in this case 2 )
     mysqli_stmt_bind_param($stmt, "ss", $username, $email);
+    //we execute the statement with statement in parameter
     mysqli_stmt_execute($stmt);
 
+    //we make the result into a variable
     $resultData = mysqli_stmt_get_result($stmt);
 
+
+    //we make a varialble "row" to store the results we fetch
     if($row = mysqli_fetch_assoc($resultData)){
-return $row;
-    } else {
+    //if row is true, than we return row, meaning we return true
+     return $row;
+      } else {
+    //else false
         $result=false;
         return $result;
     }
-
+    //we than close statement for security reasons ig
     mysqli_stmt_close($stmt);
 
 }
